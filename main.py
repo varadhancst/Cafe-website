b969143367c8cfc88ae1cb9fd34de5def5e62c99
Varadhan Elango <44795737+varadhancst@users.noreply.github.com>
2023-01-13 03:40:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -132,6 +132,6 @@ def show_cafe(cafe_id):
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+#     app.run(debug=True)
     db.create_all()
